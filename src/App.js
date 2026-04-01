88a03c724a40a8a6d0230caffc6474bbd13011e6
rahul <rahulsinghxtech@gmail.com>
2026-04-01 18:56:21 +0530

    fix: move BrowserRouter to App.js to resolve useRef runtime error

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
